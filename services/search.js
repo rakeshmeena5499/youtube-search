15e35019678dd49fe43ebe06071d15e54b5e5a3f
Rakesh Meena <rakeshmeena5499@gmail.com>
2024-01-23 22:42:48 +0530

    fixed search video functionality

--- services/search.js
+++ services/search.js
@@ -1,26 +1,27 @@
 const { google } = require("googleapis");
 const { YOUTUBE_API_KEY } = require("../config");
 
-const searchForNewVideos = (publishedAfter) => {
-  google
-    .youtube("v3")
-    .search.list({
+const searchForNewVideos = async (publishedAfter) => {
+  try {
+    const response = await google.youtube("v3").search.list({
       key: YOUTUBE_API_KEY,
       part: "snippet",
-      q: "Finance",
+      q: "Investing",
       publishedAfter: publishedAfter,
-      maxResults: 5,
-    })
-    .then((response) => {
-      console.log("Search successful...");
-      const { data } = response;
-      const videoDetails = data.items.map((item) => item.snippet);
-      console.log(videoDetails[0].thumbnails);
-    })
-    .catch((err) => {
-      console.log("Search failed!!!");
-      console.error(err);
+      maxResults: 2,
     });
+
+    console.log("Search successful...");
+    const { data } = response;
+    const videoDetails = data.items.map((item) => item.snippet);
+    console.log("Videos Details: ", videoDetails);
+
+    return videoDetails;
+  } catch (err) {
+    console.log("Search failed!!!");
+    console.error(err);
+    return searchForNewVideos(publishedAfter);
+  }
 };
 
 
